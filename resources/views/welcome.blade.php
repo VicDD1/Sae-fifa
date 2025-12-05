f92d1d0c3bae802b1b5ce20bede37b34ef9dcbb3
Zayim-moh <zayim.mohmand@etu.univ-smb.fr>
2025-12-05 08:12:42 +0000

    suppression "?"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
     <script>
         var botmanWidget = {
             aboutText: '',
-            introMessage: "Bienvenue chez FIFA. En quoi puis-je vous aider?"
+            introMessage: "Bienvenue chez FIFA. En quoi puis-je vous aider"
         };
     </script>
     <script src="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js"></script>
