1634df6399fc023f121a7c2459e24909cf442b94
mael <mael-guimoyas@etu.univ-smb.fr>
2025-12-15 13:50:45 +0000

    MES VOMMADNES

--- resources/views/succes_commande.blade.php
+++ resources/views/succes_commande.blade.php
@@ -14,7 +14,7 @@
             <p class="success-message">
                 Merci pour votre confiance. Votre commande a bien été enregistrée et est en cours de traitement.
             </p>
-            <a href="/products" class="btn-home">Retour à l’accueil</a>
+            <a href="/mes_commandes" class="btn-home">Retour à l’accueil</a>
         </div>
     </div>
 </body>
